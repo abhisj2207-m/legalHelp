16fece5faae252aa55dd01536cd19c5c58bd0b04
abhisj2207-m <abhisj2207@gmail.com>
2025-11-11 19:38:32 +0530

    initial commit

--- src/Component/Navbar.tsx
+++ src/Component/Navbar.tsx
@@ -81,7 +81,7 @@ function NavbarContent({
       {/* ===== Mobile Logo ===== */}
       <div className="flex items-center md:hidden mt-[8px]">
         <Image
-          src="/logo.png"
+          src="/logo1.png"
           alt="Find The Firm"
           width={60}
           height={35}
@@ -201,7 +201,7 @@ function NavbarContent({
               {/* Sidebar Header */}
               <div className="flex justify-between items-center p-5 border-b border-gray-200">
                 <Image
-                  src="/logo.png"
+                  src="/logo1.png"
                   alt="Find The Firm"
                   width={60}
                   height={45}
